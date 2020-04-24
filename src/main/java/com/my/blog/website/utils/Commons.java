bff533e29ee1a7474ad7b3f8577beaa641d0ecdb
HuangYuhui <Gentleman_0109@outlook.com>
2020-04-24 20:57:30 +0800

    update the Commons.java
    please refer to the commit : https://github.com/YUbuntu0109/blog/commit/c6c9211c01dc8e4ad84f45e295f91058eeb2d883

--- src/main/java/com/my/blog/website/utils/Commons.java
+++ src/main/java/com/my/blog/website/utils/Commons.java
@@ -302,9 +302,10 @@ public final class Commons {
      */
     public static String show_thumb(ContentVo contents) {
         int cid = contents.getCid();
-        int size = cid % 20;
+        //int size = cid % 20;
+        int size = cid % 40;
         size = size == 0 ? 1 : size;
-        return "/user/img/rand/" + size + ".jpg";
+        return "/user/img/rand/" + size + ".png";
     }
 
     /**
